a66d0aea2551f7816d22414fd91fe3ff7bab16e8
wagnerww <wagnerricardonet@gmail.com>
2019-10-15 13:19:44 -0300

    chore: tour vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <v-app>
-    <p>Projeto</p>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="4" class="red"></v-col>
+        <v-col cols="4" class="green"></v-col>
+        <v-col cols="4" class="blue"></v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
+//AIzaSyB-_Clu4jgIn5XJ-B92UQ2MZlYTi5GGm7E
 export default {
   name: "App"
 };
